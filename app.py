6b7b9ea61d4628185719ac17d6757084856177b4
Pooja <Poojadas93@outlook.com>
2022-09-29 18:57:53 +0530

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, url_for, redirect
 import joblib
 import pandas as pd
+import numpy as np
+import sklearn
+import gunicorn
 
 
 # load our model file
-model = joblib.load('model.xgb')
-scale = joblib.load('Scalized.scl')
+model = joblib.load('model.gbr')
+scale = joblib.load('scaler.sc')
 
 # Object for Flask
 app = Flask(__name__)
@@ -26,62 +29,29 @@ def predict():
 
         sex = int(request.form['sex'])
 
-        TSH_measured = int(request.form['TSH measured'])
+        bmi = request.form['bmi']
 
-        if TSH_measured == 0:
-            TSH = 0
-        else:
-            TSH = request.form['TSH']
+        children = request.form['children']
 
-        T3_measured = int(request.form['T3 measured'])
+        smoker = int(request.form['smoker'])
 
-        if T3_measured == 0:
-            T3 = 0
-        else:
-            T3 = request.form['T3']
-
-        TT4_measured = int(request.form['TT4 measured'])
-
-        if TT4_measured == 0:
-            TT4 = 0
-        else:
-            TT4 = request.form['TT4']
-
-        T4U_measured = int(request.form['T4U measured'])
-
-        if T4U_measured == 0:
-            T4U = 0
-        else:
-            T4U = request.form['T4U']
-
-        FTI_measured = int(request.form['FTI measured'])
-
-        if FTI_measured == 0:
-            FTI = 0
-        else:
-            FTI = request.form['FTI']
 
         # 2. create DataFrame
-        data = [[age, sex, TSH_measured, TSH, T3_measured, T3, TT4_measured, TT4, T4U_measured, T4U, FTI_measured, FTI]]
-        column_name = ['age', 'sex', 'TSH_measured', 'TSH', 'T3_measured', 'T3', 'TT4_measured', 'TT4', 'T4U_measured',
-                       'T4U', 'FTI_measured', 'FTI']
+        data = [[age, sex, bmi, children, smoker]]
+        column_name = ['age', 'sex', 'bmi', 'children', 'smoker']
 
         data_model = pd.DataFrame(data, columns=column_name)
 
         # scalized
-        num_col = ['age', 'TSH', 'T3', 'TT4', 'T4U', 'FTI']
+        num_col = ['age', 'bmi', 'children']
         data_model[num_col] = scale.transform(data_model[num_col])
 
         # Prediction
         result = model.predict(data_model)[0]
 
         # 3. Display
-        if result == 0:
-            return render_template('index.html', Thyroid_Text='Have Hyperthyroidism')
-        elif result == 1:
-            return render_template('index.html', Thyroid_Text='Have Hypothyroidism')
-        else:
-            return render_template('index.html', Thyroid_Text='Thyroid Negative')
+
+        return render_template('index.html', Predict_Text=f'Predict Premium is {result}')
 
 
 if __name__ == "__main__":
